3fa5ec41db7e1cce57bc43c56f9324f622f5dc5a
Ghenadie Pusca <g.pusca@eyeo.com>
2021-03-18 19:01:29 +0200

    Exercice 1-12
    - Print each word on a new line;

---
+++ 1_12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+   #define IN   1  /* inside a word */
+   #define OUT  0  /* outside a word */
+   /* count lines, words, and characters in input */
+int main() {
+    int c, state;
+    state = OUT;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            if (state == IN && c != '\n') {
+                putchar('\n');
+            }
+            state = OUT;
+            continue;
+        } else if (state == OUT) {
+            state = IN;
+        }
+        putchar(c);
+    }
+}
